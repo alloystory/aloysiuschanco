c62dbc903922bd2e6b12c64d157119039d0bc537
Aloysius Chan <alloystory@gmail.com>
2020-10-27 01:45:01 +0800

    revamp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const gulpRename = require("gulp-rename");
+const gulpUtil = require("gulp-util")
+
+function sass(callback) {
+    gulp.src("src/scss/style.scss")
+        .pipe(gulpSass())
+        .on("error", gulpUtil.log)
+        .pipe(gulp.dest("src/css"))
+    callback()
+}
+
+function css(callback) {
+    // gulp.src("node_modules/bootstrap/dist/css/bootstrap.min.css")
+    //     .pipe(gulp.dest("src/css"))
+    // gulp.src("node_modules/@fortawesome/fontawesome-free/css/brands.min.css")
+    //     .pipe(gulpRename("fa-brands.min.css"))
+    //     .pipe(gulp.dest("src/css"))
+    callback()
+}
+
+function js(callback) {
+    gulp.src([
+        "node_modules/bootstrap/dist/js/*.min.js",
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/popper.js/dist/popper.min.js"
+    ])
+        .pipe(gulp.dest("src/js"))
+    callback()
+}
+
+function watchSass(callback) {
+    gulp.watch("src/scss/**/*.scss", gulp.series(sass))
+    callback()
+}
+
+exports.default = gulp.series(sass, css, js, watchSass)
