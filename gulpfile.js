cd43054c5a4d7401812219fda386cd51e339ae7f
Aloysius Chan <alloystory@gmail.com>
2020-11-01 16:02:59 +0800

    fix fa icons directory

--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,21 @@ const gulpSass = require("gulp-sass");
 const gulpCleanCSS = require("gulp-clean-css");
 const gulpUtil = require("gulp-util")
 
-function sass(callback) {
-    gulp.src("src/scss/style.scss")
-        .pipe(gulpSass())
-        .on("error", gulpUtil.log)
-        .pipe(gulp.dest("src/css"))
-    callback()
-}
-
-function js(callback) {
-    gulp.src([
-        "node_modules/bootstrap/dist/js/*.min.js",
-        "node_modules/jquery/dist/jquery.min.js",
-        "node_modules/popper.js/dist/popper.min.js"
-    ])
-        .pipe(gulp.dest("src/js"))
-    callback()
-}
-
-function watchSass(callback) {
-    gulp.watch("src/scss/**/*.scss", gulp.series(sass))
-    callback()
-}
-
 function compile(callback) {
     gulp.src("src/scss/style.scss")
         .pipe(gulpSass())
+        .on("error", gulpUtil.log)
         .pipe(gulpCleanCSS())
         .pipe(gulp.dest("dist/css"))
+
+    gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/**/*")
+        .pipe(gulp.dest("dist/assets/fa-fonts"))
 
     gulp.src([
         "node_modules/bootstrap/dist/js/*.min.js",
         "node_modules/jquery/dist/jquery.min.js",
         "node_modules/popper.js/dist/popper.min.js"
-    ])
+        ])
         .pipe(gulp.dest("dist/js"))
 
     gulp.src("src/assets/**/*")
@@ -47,5 +28,10 @@ function compile(callback) {
     callback()
 }
 
+function watchSass(callback) {
+    gulp.watch("src/scss/**/*.scss", gulp.series(compile))
+    callback()
+}
+
 exports.compile = compile
-exports.default = gulp.series(sass, js, watchSass)
+exports.default = gulp.series(compile, watchSass)
